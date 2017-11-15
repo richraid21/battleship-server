5abe2f774b54331f30215ea38059598c9bbc0987
Rich Dillon <richraid21@gmail.com>
2017-11-15 17:31:13 -0500

    Change the default user from postgres -> battleship

--- src/knexfile.js
+++ src/knexfile.js
@@ -3,7 +3,7 @@ var env = process.env
 var databaseConfig = {
     client: 'pg',
     connection: { 
-        user: env.BS_DB_USER || 'postgres',
+        user: env.BS_DB_USER || 'battleship',
         password: env.BS_DB_PASSWORD || 'pixi3',
         host: env.BS_DB_HOST || '127.0.0.1',
         port: env.BS_DB_PORT || '5432',
